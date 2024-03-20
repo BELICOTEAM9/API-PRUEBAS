d2dee106032caf7484f3987f47ea2aa1a18d0e11
Csrhdz <105759540+Csrhdz@users.noreply.github.com>
2024-03-20 00:43:05 +0000

    m

--- prueba.test.js
+++ prueba.test.js
@@ -11,8 +11,8 @@ describe('Pruebas de integración del servidor Express', () => {
 
   // Prueba para obtener el detalle de un usuario por ID
   test('Obtener detalle de un usuario por ID', async () => {
-    // Supongamos que el usuario con ID 1 existe en la base de datos
-    const userId = 1;
+    // Supongamos que el usuario con ID 2 existe en la base de datos
+    const userId = 2;
     const response = await request(app).get(`/users/${userId}`);
     expect(response.statusCode).toBe(200);
     expect(response.body).toBeTruthy(); // Verifica que haya algún contenido en la respuesta
@@ -20,7 +20,7 @@ describe('Pruebas de integración del servidor Express', () => {
 
   // Prueba para crear un nuevo usuario
   test('Crear un nuevo usuario', async () => {
-    const newUser = { name: 'Juan', email: 'juan@example.com' };
+    const newUser = { name: 'Luis', email: 'Luis@example.com' };
     const response = await request(app).post('/users').send(newUser);
     expect(response.statusCode).toBe(201);
     expect(response.body).toHaveProperty('id');
@@ -31,7 +31,7 @@ describe('Pruebas de integración del servidor Express', () => {
   // Prueba para actualizar un usuario existente
   test('Actualizar un usuario existente', async () => {
     // Supongamos que el usuario con ID 1 existe en la base de datos
-    const userId = 1;
+    const userId = 2;
     const updatedUser = { name: 'Pedro', email: 'pedro@example.com' };
     const response = await request(app).put(`/users/${userId}`).send(updatedUser);
     expect(response.statusCode).toBe(200);
@@ -41,7 +41,7 @@ describe('Pruebas de integración del servidor Express', () => {
   // Prueba para eliminar un usuario existente
   test('Eliminar un usuario existente', async () => {
     // Supongamos que el usuario con ID 1 existe en la base de datos
-    const userId = 1;
+    const userId = 7;
     const response = await request(app).delete(`/users/${userId}`);
     expect(response.statusCode).toBe(200);
     expect(response.body).toHaveProperty('message', 'Usuario eliminado exitosamente');
